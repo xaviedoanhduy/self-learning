6071aac039a9509590adc790efcb216a83afb9ee
Duy (Đỗ Anh) <doanhduyxavie@gmail.com>
2025-11-24 22:12:11 +0700

    fix: missing change icon feature in use-case-05

--- owl/modules/use_case_05/static/src/widget/smiley_timer.js
+++ owl/modules/use_case_05/static/src/widget/smiley_timer.js
@@ -1,6 +1,6 @@
 import { patch } from "@web/core/utils/patch";
 import { onMounted, onWillDestroy } from "@odoo/owl";
-import { SmileyWidget } from "@use_case_03/widget/smiley_widget";
+import { SmileyWidget, smileyWidget } from "@use_case_03/widget/smiley_widget";
 
 patch(SmileyWidget.prototype, {
     setup() {
@@ -10,7 +10,7 @@ patch(SmileyWidget.prototype, {
         onMounted(() => {
             this.timer = setTimeout(() => {
                 this.state.isAngry = true; 
-            }, 5000); // 5000 is only for testing - while according to the exercise 1 minute corresponds to 60000
+            }, 10000); // 10000 is only for testing - while according to the exercise 1 minute corresponds to 60000
         });
 
         onWillDestroy(() => {
@@ -30,4 +30,8 @@ patch(SmileyWidget.prototype, {
         }
         super.toggleMood();
     }
+});
+
+patch(smileyWidget, {
+    template: "use_case_05.SmileyTimer",
 });
