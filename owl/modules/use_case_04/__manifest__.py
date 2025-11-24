bab5e5a3cec3aabfc71ac2e37b49788cb4039b0c
Duy (Đỗ Anh) <doanhduyxavie@gmail.com>
2025-11-24 21:18:48 +0700

    feat: add use-case-04: Dont touch

---
+++ owl/modules/use_case_04/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    "name": "Use Case 04: Dont Touch",
+    "summary": "Shows a warning when clicking the smiley face.",
+    "description": "Updates the smiley widget to show a 'Get back to work!' notification when clicked, as requested by the Manager.",
+    "author": "Do Anh Duy",
+    "maintainer": "xaviedoanhduy",
+    "website": "https://github.com/xaviedoanhduy/self-learning",
+    "category": "Sales",
+    "version": "18.0.1.0.0",
+    "depends": ["use_case_03"],
+    "data": [],
+    "assets": {
+        "web.assets_backend": [
+            "use_case_04/static/src/widget/smiley_extension.js",
+        ]
+    },
+    "application": True,
+    "installable": True,
+    "license": "LGPL-3"
+}
